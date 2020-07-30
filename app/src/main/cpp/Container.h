5e1a7bfd1f269967ea3aeed8dc85816cd90e8b2f
Rian Prakoso <mohammad.rian.ap@gmail.com>
2020-07-30 17:04:39 +0700

    success drawing rectangle

---
+++ app/src/main/cpp/Container.h
@@ -0,0 +1,54 @@
+//
+// Created by Rian Prakoso on 7/30/20.
+//
+
+#ifndef COMPASS_CONTAINER_H
+#define COMPASS_CONTAINER_H
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+#include <android/sensor.h>
+#include <android/log.h>
+#include <android_native_app_glue.h>
+#include <cairo.h>
+#include <cairo-gl.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+
+struct Container {
+    EGLint width, height;
+    EGLSurface surface;
+    EGLContext context;
+    EGLDisplay display;
+    cairo_device_t* cdt;
+    cairo_surface_t* crSurface;
+    cairo_t* cr;
+    android_app* app;
+    bool running;
+    bool animating;
+
+    int initEgl();
+    int deinitEgl();
+};
+
+
+//struct Container {
+//    EGLint width, height;
+//    EGLSurface surface;
+//    EGLContext context;
+//    EGLDisplay display;
+//    cairo_device_t* cdt;
+//    cairo_surface_t* crSurface;
+//    cairo_t* cr;
+//    android_app* app;
+//    bool running;
+//    bool animating;
+//};
+//
+//
+//int initEgl(Container& container1);
+//int deinitEgl(Container& container1);
+
+
+#endif //COMPASS_CONTAINER_H
